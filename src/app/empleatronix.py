a97b621b852735f787a814104cbaae5dd1476b60
albertomorenogonzalez <albertomorenogonzalez03@gmail.com>
2024-01-11 10:49:55 +0100

    data access

---
+++ src/app/empleatronix.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('EMPLEATRONIX')
+st.subheader('Todos los datos sobre los empleados en una aplicación.')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv("src\data\employees.csv", nrows=nrows)
+    return data
+
+data = load_data()
+
+if st.checkbox('Mostrar datos de empleados'):
+    st.subheader('Datos de los Empleados')
+    st.write(data)
