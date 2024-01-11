2e22e289d3efae34c5706af1ed049ce3aa541531
albertomorenogonzalez <albertomorenogonzalez03@gmail.com>
2024-01-11 13:22:51 +0100

    bar hist config

--- src/empleatronix.py
+++ src/empleatronix.py
@@ -16,3 +16,16 @@ data = load_data()
 if st.checkbox('Mostrar datos de empleados'):
     st.subheader('Datos de los Empleados')
     st.write(data)
+
+st.divider()
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    color = st.color_picker('Elige un color para las barras', '#3475B3')
+
+with col2:
+    show_name = st.toggle('Mostrar el nombre')
+
+with col3:
+    show_salary = st.toggle('Mostrar el sueldo en la barra')
