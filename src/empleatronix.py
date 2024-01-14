21a6a4cf0aff7be9a09f7a134216806047f9a4c3
Alberto <albertomorenogonzalez03@gmail.com>
2024-01-14 15:04:50 +0100

    Fixing graphic behaviour

--- src/empleatronix.py
+++ src/empleatronix.py
@@ -25,19 +25,27 @@ with col1:
     color = st.color_picker('Elige un color para las barras', '#3475B3')
 
 with col2:
-    show_name = st.toggle('Mostrar el nombre')
+    name = st.toggle('Mostrar el nombre')
 
 with col3:
-    show_salary = st.toggle('Mostrar el sueldo en la barra')
+    salary = st.toggle('Mostrar el sueldo en la barra')
 
 
 fig, ax = plt.subplots()
 
-if show_name:
-    bars = ax.barh(data['full name'], data['salary'], color=color)
-    ax.bar_label(bars, padding=5)
-else:
-    bars = ax.barh(data['full name'], data['salary'], color=color)
-    ax.yaxis.set_visible(False)
-    ax.set_ylim([-0.5, len(data['full name']) - 0.5])
+bars = ax.barh(data['full name'], data['salary'], color=color)
+
+def show_name(name, bars):
+    if not name:
+        ax.yaxis.set_visible(False)
+        ax.set_ylim([-0.5, len(data['full name']) - 0.5])
+
+    show_salary(salary, bars)
+
+def show_salary(salary, bars):
+    if salary:
+        ax.bar_label(bars, padding=5)
+
+show_name(name, bars)
+
 st.pyplot(fig)
